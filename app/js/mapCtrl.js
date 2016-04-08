3e09205dfa6db31a06c9d8b4383ecdb87ab2c315
hertzan <hertz.marcus@gmail.com>
2016-04-08 16:02:19 +0200

    fix map

--- app/js/mapCtrl.js
+++ app/js/mapCtrl.js
@@ -2,7 +2,7 @@ tweetmapApp.controller('MapCtrl', function ($scope, NgMap) {
 	var myMap = this;
 	$scope.placeChanged = function() {
 		myMap.place = this.getPlace();
-		console.log('location', myMap.place.geometry.location.lat());
+		console.log('location','lat:', myMap.place.geometry.location.lat(),'lng:' ,myMap.place.geometry.location.lng());
 		myMap.map.setCenter(myMap.place.geometry.location);
 	}
 	NgMap.getMap().then(function(map) {
