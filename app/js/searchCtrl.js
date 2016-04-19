abaf5444dd681199534d373be8dfa8f0bdeb260c
hertzan <hertz.marcus@gmail.com>
2016-04-19 10:24:51 +0200

    starting with tweetList

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -3,5 +3,6 @@ tweetmapApp.controller('SearchCtrl', function ($scope, factory) {
 
 	$scope.search = function(query) {
 		factory.getGeoSearch(query);
+		$scope.foundTweets = factory.getTrendsArray();
 	}
 });
