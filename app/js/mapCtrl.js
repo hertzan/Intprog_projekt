59b0c37b4b53672a8ae3f2f77c86a5cbb8ff3a72
hertzan <hertz.marcus@gmail.com>
2016-04-23 23:33:18 +0200

    working still on coodinates and how to print on map

--- app/js/mapCtrl.js
+++ app/js/mapCtrl.js
@@ -8,6 +8,8 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 
 	updateTrends();
 
+	
+
 	$scope.goToSearchPage = function(hashtag) {
 		factory.setHashtag(hashtag.word);
 		factory.setPosition(hashtag.pos);
@@ -33,15 +35,15 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 				// only plot markers for those not currently in bounds
 				if(currentCities.indexOf(citiesInBounds[i]) == -1){
 					// TODO plotta för stad som ej syns
-					plotStuff(citiesInBounds[i]);
-					//console.log(currentCities[i]);
+					//plotStuff(citiesInBounds[i]);
+					console.log("currentCities before: ");
+					console.log(citiesInBounds[i]);
 					console.log("plotting for: " + citiesInBounds[i].name);
+					$scope.tweetsWithPos = getPlotCoordinates(citiesInBounds[i]);
 				}
 			}
 			// update the current cities with those in bounds
 			currentCities = citiesInBounds;
-			//console.log("currentCities: ");
-			//console.log(currentCities);
 
 			lat = center.lat();
 			long = center.lng();
@@ -64,12 +66,16 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 		//myMap.map.customMarkers.foo.setPosition(lat, long);
 	}
 
-	function getPlotCoordinates(city, array) {
+	function getPlotCoordinates(city) {
 		var plotCoordinates = new Array();
+		var retreivedArray = new Array();
 		var plotlat = city.latitude;
 		var plotlong = city.longitude;
-		for(var i=0;i<array.length;i++) {
-			plotCoordinates.push([array[i], plotlat, plotlong]);
+
+		retreivedArray = recursiveGetCalls(5, plotlat, plotlong, plotCoordinates, null);
+
+		for(var i=0;i<retreivedArray.length;i++) {
+			plotCoordinates.push([name[retreivedArray[i]], pos[plotlat,plotlong]]);
 			//ingen aning vad detta kommer plotta men man måste testa något hehe...
 			if(i % 2 == 0){
 				plotlat + 1;
@@ -113,6 +119,7 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 		factory.getTrends().then(function(foundTweets) {
 			console.log("updating trends:");
 			console.log(foundTweets);
+			trendsArr = foundTweets.trends;
 
 			$scope.trends=foundTweets[0].trends.slice(0,19);
 
@@ -128,16 +135,16 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 
 		updatePlace();
 
-		var recursiveArray = new Array();
+		//var recursiveArray = new Array();
 		//recursiveGetCalls(0, recursiveArray, null);
 	}
 
 	// recursive function for making 10 independent get Search/Tweet calls, appending
 	// the total array of results.
-	function recursiveGetCalls(index, array, max_id){
+	function recursiveGetCalls(index, latitude, longitude, array, max_id){
 		if(index < 10){
 			// make the API call and update trends list and map markers
-			factory.getSearchTweets("#", '"'+lat+', '+long+', 10km"',"100", max_id).then(function(foundTweets) {
+			factory.getSearchTweets("#", '"'+latitude+', '+longitude+', 10km"',"100", max_id).then(function(foundTweets) {
 				array = array.concat(foundTweets.statuses);
 
 				// search for a new max id
@@ -146,7 +153,7 @@ tweetmapApp.controller('MapCtrl', function ($scope, factory, NgMap) {
 				console.log("finished call no: " + index);
 
 				// do next call with updated index, array, and max_id
-				recursiveGetCalls(index+1, array, max_id);
+				recursiveGetCalls(index+1, latitude, longitude, array, max_id);
 			});
 		}
 		if(index == 10) {
